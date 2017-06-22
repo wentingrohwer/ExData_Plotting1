a0dcc971e3c0d22d3e3dc801ef2e85ccd4f39022
Wenting Rohwer <wentingr@isoftstone.com>
2017-06-21 18:34:14 -0700

    finished 3 and 4

---
+++ plot3.R
@@ -0,0 +1,10 @@
+with(df, {
+  plot(Sub_metering_1~DateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  lines(Sub_metering_2~DateTime, col='Red')
+  lines(Sub_metering_3~DateTime, col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.8)
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,18 @@
+#plot4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(df, {
+  plot(Global_active_power~DateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  plot(Voltage~DateTime, type="l", 
+       ylab="Voltage (volt)", xlab="", cex=0.8)
+  plot(Sub_metering_1~DateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  lines(Sub_metering_2~DateTime,col='Red')
+  lines(Sub_metering_3~DateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.5)
+  plot(Global_reactive_power~DateTime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="", cex=0.8)
+})
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
